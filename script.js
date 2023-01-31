ac67ba570866f2e815e7b5b37468cd52830a916a
Pedro Alencar <pedromariutti@gmail.com>
2023-01-31 17:39:19 -0300

    Javascript for checkin passwords match

---
+++ script.js
@@ -0,0 +1,23 @@
+const pwd = document.querySelector('#pwd');
+const pwdConf = document.querySelector('#pwd-conf');
+
+const pwdCheck = function(pss){
+    return pss.value
+}
+
+
+const confirm = function(){   
+    const textConfirm = document.querySelector("#pwd-conf~p")
+    if (pwdCheck(pwd) !== pwdCheck(pwdConf)) {
+        textConfirm.textContent = "passwords do not match"
+        pwdConf.style.border = "1px solid red";
+    } else {
+        textConfirm.textContent = ""
+        pwdConf.style.border = "1px solid #E5E7EB"
+    }
+    textConfirm.style.color = 'red';
+    textConfirm.style.fontSize = "0.7rem";
+    textConfirm.style.textTransform = 'uppercase';
+}
+
+confirm()
